797ff6bb0412d35b4ecba48ae4918a29986fc5f5
Jason <jljasonlin1101@gmail.com>
2023-06-28 12:57:49 -0400

    Created First Draft with results in Debug Console

---
+++ index.js
@@ -0,0 +1,41 @@
+const genMotivate = () => {
+    let x = Math.floor(Math.random() * 5);
+    switch (x) {
+        case 0:
+            console.log("Mov1");
+            break;
+        case 1:
+            console.log("Mov1");
+            break;
+        case 2:
+            console.log("Mov1");
+            break;
+        case 3:
+            console.log("Mov1");
+            break;
+        case 4:
+            console.log("Mov1");
+            break;      
+    }
+}
+
+const genFunny = () => {
+    let x = Math.floor(Math.random() * 5);
+    switch (x) {
+        case 0:
+            return "Mov1"
+            break;
+        case 1:
+            return "Mov2"
+            break;
+        case 2:
+            return "Mov"
+            break;
+        case 3:
+            return "Mov"
+            break;
+        case 4:
+            return "Mov"
+            break;      
+    }
+}
