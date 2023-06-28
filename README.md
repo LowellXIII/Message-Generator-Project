ab2ff61697566e8515b0f0b1a45250e56f84a604
Jason <jljasonlin1101@gmail.com>
2023-06-28 15:13:52 -0400

    Working Draft

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Message -Generator-Project
+##Offers two buttons for users to play with
+###A Motivation and a Funny Button
+
+###The goal is to generate a random message when user clicks on either button
+###The message will be based on the topic of the button
--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 const genMotivate = () => {
     let x = Math.floor(Math.random() * 5);
+    document.getElementById("result").style.backgroundColor = "white";
+    let str = "";
     switch (x) {
         case 0:
-            console.log("Mov1");
+            str = "Don't give up!"
             break;
         case 1:
-            console.log("Mov1");
+            str = "Learning is supposed to be stressful, but that's how the brain gets stronger, just like our muscles"
             break;
         case 2:
-            console.log("Mov1");
+            str = "It's okay to break apart, because you'll know to how build yourself better and stronger next time."
             break;
         case 3:
-            console.log("Mov1");
+            str = "Get up, a man doesn't stare at the ground, but instead, of what's ahead."
             break;
         case 4:
-            console.log("Mov1");
+            str = "No matter the obsticle, destroy, them, all."
             break;      
     }
+    document.getElementById("result").innerHTML = str;
 }
 
 const genFunny = () => {
     let x = Math.floor(Math.random() * 5);
+    document.getElementById("result").style.backgroundColor = "white";
+    let str = "";
     switch (x) {
         case 0:
-            return "Mov1"
+            str = "Fat                   Cat"
             break;
         case 1:
-            return "Mov2"
+            str = "Just laugh"
             break;
         case 2:
-            return "Mov"
+            str = "What do Asians say when you get a B in Math? Nothing, they just disown you... -Jason Lin"
             break;
         case 3:
-            return "Mov"
+            str = "Feeling heartbroken from being dumpped? That's okay, 9 cats are better than that person"
             break;
         case 4:
-            return "Mov"
+            str = "One day, I will run a 5 minute mile"
             break;      
     }
+    document.getElementById("result").innerHTML = str;
 }
